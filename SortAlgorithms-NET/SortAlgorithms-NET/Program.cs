46df7bfeba7140343ea529c6c1d74c25f8379ba6
Leonid Boginski <65702576+leonidbogin@users.noreply.github.com>
2020-10-08 19:14:12 +0300

    Add array input & output

--- SortAlgorithms-NET/SortAlgorithms-NET/Program.cs
+++ SortAlgorithms-NET/SortAlgorithms-NET/Program.cs
@@ -6,6 +6,24 @@ namespace SortAlgorithms_NET
     {
         static void Main(string[] args)
         {
+            int[] array = new int[] { 10, 5, 1, 7, 4, 9, 8, 6, 2, 3 };
+            Console.Write("Before: ");
+            Print(array);
+
+            Console.ReadLine();
+        }
+
+        static void Print<T>(T[] array)
+        {
+            if (array.Length > 0)
+            {
+                Console.Write(array[0]);
+                for (int i = 1; i < array.Length; i++)
+                {
+                    Console.Write(" " + array[i]);
+                }
+                Console.WriteLine();
+            }
         }
     }
 }
